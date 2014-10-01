27ac8123b092f8d649ba91d90219b756c2ad2c88
Max Patsiansky <m.patsianskiy@zmeke.com>
2014-10-01 18:12:40 +0400

    chapter 8.5 done

--- collections/posts.js
+++ collections/posts.js
@@ -1,5 +1,17 @@
 Posts = new Meteor.Collection('posts');
 
+Posts.allow({
+  update: ownsDocument,
+  remove: ownsDocument
+});
+
+Posts.deny({
+  update: function(userId, post, fieldNames) {
+    // разрешаем редактировать только следующие два поля:
+    return (_.without(fieldNames, 'url', 'title').length > 0);
+  }
+});
+
 Meteor.methods({
   post: function(postAttributes) {
     var user = Meteor.user(),
@@ -21,14 +33,23 @@ Meteor.methods({
     }
 
     // Выберем поля разрешенные для публикации
-    var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
+    var post = _.extend(_.pick(postAttributes, 'url', 'message'), {
+      title: postAttributes.title + (this.isSimulation ? '(client)' : '(server)'),
       userId: user._id,
       author: user.username,
       submitted: new Date().getTime()
     });
 
-    var postId = Posts.insert(post);
+    if (! this.isSimulation) {
+      var Future = Npm.require('fibers/future');
+      var future = new Future();
+      Meteor.setTimeout(function() {
+        future.return();
+      }, 5 * 1000);
+      future.wait();
+    }
 
+    var postId = Posts.insert(post);
     return postId;
   }
 });
